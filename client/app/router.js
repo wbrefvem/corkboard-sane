3f781bb744956bac1cf3254966e3e673502cc255
Will Refvem <wbrefvem@gmail.com>
2015-04-02 16:46:55 -0400

    More style changes

--- client/app/router.js
+++ client/app/router.js
@@ -6,17 +6,22 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+
   this.resource('special-events', function() {
+    
     this.route('new', function() {
+      
       this.route('contact-info');
       this.route('event-details');
       this.route('zones-and-usage');
       this.route('disclaimers-and-review');
     });
+    
     this.route('list');
   });
   
   this.resource('special-event', { path: '/special-events/:special-event_id' }, function() {
+    
     this.route('edit');
     this.route('delete');
   });
